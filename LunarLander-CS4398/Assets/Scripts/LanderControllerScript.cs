dfbebcf67c4f1bfedeed308742ab5d3fe621b2a5
tkudlek <tmk54@txstate.edu>
2015-07-18 20:06:12 -0500

    Fuel Gauge Addition
    A fuel gauge was added to the bottom left hand side of the screen. This
    game object follows the 2D camera around so it stays put. An arbitrary
    fuel amount was added to the Lunar Lander script, and it was linked to a
    fuel gauge class. As the user uses thrust, the fuel will deplete and a
    visual representation is updated. Once the fuel gauge is empty, then the
    thrusters halt operation.

--- LunarLander-CS4398/Assets/Scripts/LanderControllerScript.cs
+++ LunarLander-CS4398/Assets/Scripts/LanderControllerScript.cs
@@ -4,99 +4,120 @@ using System.Collections;
 
 
 public class LanderControllerScript : MonoBehaviour {
-
-
+	
+	
 	public GameObject thrusters;
 	public GameObject leftThruster;
 	public GameObject rightThruster;
+	public GameObject fuelGauges;
 
 	public float thrustAcceleration = 5f;
 	public float thrustRotation = 5f;
 	public float maxSpeed = 10f;
-
+	public int fuelAmount = 5000; //an arbitrary amount of fuel for the lander
+	
 	private Rigidbody2D landerRigidBody;
-
-
-
-
-
+	
+	
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 		hideThrusters ();
 		landerRigidBody = GetComponent<Rigidbody2D>();
 	}
-
-
+	
+	
 	// This function is called every time the physics engine updates. Because the Lander
 	// uses physics, FixedUpdate() should be called instead of Update()
 	void FixedUpdate () {
-
+		
 		if (Input.GetKeyDown ("r")) {
 			print ("space key was pressed");
 
 			Application.LoadLevel(Application.loadedLevelName);
-
+			
 			return;
 		}
-
+		
 		// Get Left/Right Input. This will be used to rotate the Lander.
 		float rotate = Input.GetAxisRaw ("Horizontal");
-
+		
 		if (rotate != 0) {
-			applyRotation (rotate);
 
 			// User is rotating clockwise
 			if(rotate > 0)
 			{
-				showLeftThruster();
+				if(fuelAmount <= 0)
+					hideLeftThruster();
+				else
+				{
+					applyRotation(rotate);
+					showLeftThruster();
+					fuelAmount = fuelAmount - 1;
+				}
 			}
 			else
 			{
-				showRightThruster();
+				if(fuelAmount <= 0)
+					hideRightThruster();
+				else
+				{
+					applyRotation(rotate);
+					showRightThruster();
+					fuelAmount = fuelAmount - 1;
+				}
 			}
-
+			
 		} else {
 			hideLeftThruster();
 			hideRightThruster();
 		}
-
+		
 		// Get Up/Down Input. This will be used to rotate the Lander.
 		float thrust = Input.GetAxisRaw ("Vertical");
 		if (thrust > 0f) {
-			applyThrust(thrust);
-			showThrusters();
+			if(fuelAmount <= 0)
+				hideThrusters();
+			else
+			{
+				applyThrust(thrust);
+				showThrusters();
+				fuelAmount = fuelAmount - 10;
+			}
 		} 
 		else {
 			hideThrusters();
 		}
-
+		
 	}
-
-
-
-
+	
+	
+	
+	
 	void applyThrust(float thrust)
 	{
 		landerRigidBody.AddForce(transform.up * thrustAcceleration);
-
+		
 		//float upDirection = Vector2.up;
 		//landerRigidBody.AddForce(Vector3.up * thrustAcceleration * Time.deltaTime);
 	}
-
+	
 	void applyRotation(float rotation)
 	{
-//		Debug.Log ("Rotation = " + transform.rotation.eulerAngles.z);
-
+		//		Debug.Log ("Rotation = " + transform.rotation.eulerAngles.z);
+		
 		float initialRotation = transform.rotation.eulerAngles.z;
 		float finalRotation = initialRotation + (-rotation * thrustRotation);
-
+		
 		// This Lerp function slowly applies the rotation over time
 		transform.rotation = Quaternion.Lerp ( transform.rotation, Quaternion.Euler(0,0,finalRotation), Time.deltaTime*thrustRotation);
 	}
-
-
+	
+	
 	// These two functions show and hide the Thrusters GameObject in the Lander
-
+	
 	void showThrusters()
 	{
 		thrusters.SetActive(true);
@@ -128,4 +149,8 @@ public class LanderControllerScript : MonoBehaviour {
 	}
 
 
+
+
+	
+	
 }
---
+++ LunarLander-CS4398/Assets/Scripts/fuelGauges.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fuelGauges : MonoBehaviour {
+
+	public LanderControllerScript lander;
+	private float MAX_FUEL;
+	private float scale = 1;
+	private Sprite fuelIndicator;
+
+	// Use this for initialization
+	void Start () {
+		MAX_FUEL = 5000;
+		fuelIndicator = GetComponent<Sprite>();
+		transform.localScale = new Vector3(scale, 1, 1);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scale = (lander.fuelAmount / MAX_FUEL);
+		transform.localScale = new Vector3(scale, 1, 1);
+	
+	}
+}
