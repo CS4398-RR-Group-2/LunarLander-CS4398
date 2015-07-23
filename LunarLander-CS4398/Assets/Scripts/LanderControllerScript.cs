b6c7a53ab96ebb128ec6f158e78187f958cb2a65
jlf123 <jlf123@txstate.edu>
2015-07-23 17:31:18 -0500

    updated
    made class for score

--- LunarLander-CS4398/Assets/Scripts/LanderControllerScript.cs
+++ LunarLander-CS4398/Assets/Scripts/LanderControllerScript.cs
@@ -208,9 +208,14 @@ public class LanderControllerScript : MonoBehaviour {
 		yVelocity = Mathf.Abs (landerRigidBody.velocity.y);
 
 		if ((xVelocity > DAMAGE_THRESHOLD || yVelocity > DAMAGE_THRESHOLD) && healthAmount > 0) {
-			healthAmount = healthAmount - DAMAGE_MULTIPLIER * Mathf.Pow ((xVelocity + yVelocity),2);
+			healthAmount = healthAmount - DAMAGE_MULTIPLIER * Mathf.Pow ((xVelocity + yVelocity), 2);
+
+			ScoreManager.score -= 10;
+
 			if (healthAmount <= 0)
 				healthAmount = 0; //game loss sequence should occur
+		} else {
+			ScoreManager.score += 10;
 		}
 	}
 
---
+++ LunarLander-CS4398/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+
+	public Text ScoreText;
+	static public int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		ScoreText = GetComponent<Text>();
+		ScoreText.text = "Score : " + score.ToString();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreText.text = "Score : " + score.ToString();
+	
+	}
+
+
+
+
+}
