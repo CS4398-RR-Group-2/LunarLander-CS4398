e35649f101663e32301712f6b8df2d45a275b228
jnb58txstate <jnb58txstate@gmail.com>
2015-07-29 20:34:53 -0500

    save

---
+++ LunarLander-CS4398/Assets/Scripts/ExplodeOnContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public float explosionLifetime = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {		
+		// Spawn Object
+		GameObject newMeteor = (GameObject) Instantiate (explosion, this.gameObject.transform.position, this.gameObject.transform.rotation);
+		// Destroy the meteor after it's lifetime ends
+		Destroy(newMeteor, explosionLifetime); 
+
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ LunarLander-CS4398/Assets/Scripts/MoveTowardTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardTarget : MonoBehaviour {
+
+	private Rigidbody2D thisRigidBody;
+	public Transform target;
+	public float movementSpeed = 50;
+
+	// Use this for initialization
+	void Start () {
+		thisRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector2 targetVector = target.position - this.transform.position;
+		targetVector *= movementSpeed * Time.deltaTime;
+
+
+		thisRigidBody.AddForce (targetVector);
+	}
+}
